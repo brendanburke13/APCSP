ab9be12aa9c49be61d8d04bb493b24981cc85b8e
Brendan Burke <brendanburke@ORRHS.ORG>
2020-02-06 13:57:13 -0500

    initial

---
+++ app.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+background('yellow')
+createP('this is a cute picture of me')
+
+}
+
+function draw() {
+    createP('This is me')
+}
